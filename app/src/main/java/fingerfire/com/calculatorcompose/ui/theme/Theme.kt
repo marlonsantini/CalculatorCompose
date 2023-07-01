bbf84b861520a61a14cc9851cf22d05718d17918
Marlon Santini <marlonsantini@gmail.com>
2023-06-30 23:45:31 -0300

    first commit

---
+++ app/src/main/java/fingerfire/com/calculatorcompose/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package fingerfire.com.calculatorcompose.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    onPrimary = Purple700,
+    secondary = Teal200,
+    background = Color.DarkGray
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500,
+    onPrimary = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun CalculatorComposeTheme(
+    darkTheme: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
